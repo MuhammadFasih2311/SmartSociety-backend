cf24bb3b9aa3de986d8db785ca19df7c7a5d66d7
Fasih <fasih2412@gmail.com>
2026-08-19 16:31:10 +0500

    Fix: Force database name to smartsociety

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/smartsociety', {
+      dbName: 'smartsociety',
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
